7948cf1fdddc3e7e04d8665f0e160d0406ed45e0
mayurianu23 <tarlemayuri74@gmail.com>
2024-12-31 23:33:36 +0530

    adding 03_todo_models_creation

---
+++ 03_todo_models_creation/models/user.models.js
@@ -0,0 +1,26 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      // required: [true, "password is required"]
+    },
+  },
+  { timestamps: true }
+);
+
+export const User = monggose.model('User', userSchema);
